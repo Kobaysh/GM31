1b303563f3c4b9a5865215402cdbc4f0daf43dc2
Kobaysh <73707817+Kobaysh@users.noreply.github.com>
2021-05-26 19:05:44 +0900

    リスト構造にした

--- manager.cpp
+++ manager.cpp
@@ -7,6 +7,7 @@
 #include "Field.h"
 #include "model.h"
 #include "Player.h"
+#include <list>
 
 // 静的変数宣言
 //Manager* Manager::instance;
@@ -15,20 +16,22 @@
 //Field* g_Field = NULL;
 //Player* g_Player = NULL;
 
-enum GAMEOBJECT_TYPE
+/*enum GAMEOBJECT_TYPE
 {
 	CAMERA,
 	POLYGON,
 	FIELD,
 	PLAYER,
 	MAX,
-};
+};*/
 //GameObject* g_polygon = NULL;
 //GameObject* g_Camera = NULL;
 //GameObject* g_Field = NULL;
 //GameObject* g_Player = NULL;
 
-GameObject* g_pGameObject[GAMEOBJECT_TYPE::MAX] = {};
+//GameObject* g_pGameObject[GAMEOBJECT_TYPE::MAX] = {};
+std::list<GameObject*> g_GameObject;
+
 //
 //void Manager::Init()
 //{
@@ -72,22 +75,47 @@ void ManagerT::Init()
 	g_Field->Init();
 	g_Player = new Player();
 	g_Player->Init();*/
-	g_pGameObject[POLYGON] = new Polygon2D();
+	/*g_pGameObject[POLYGON] = new Polygon2D();
 	g_pGameObject[POLYGON]->Init();
 	g_pGameObject[CAMERA] = new Camera();
 	g_pGameObject[CAMERA]->Init();
 	g_pGameObject[FIELD] = new Field();
 	g_pGameObject[FIELD]->Init();
 	g_pGameObject[PLAYER] = new Player();
-	g_pGameObject[PLAYER]->Init();
+	g_pGameObject[PLAYER]->Init();*/
+
+	Camera* camera = new Camera();
+	camera->Init();
+	g_GameObject.push_back(camera);
+
+	Field* field = new Field();
+	field->Init();
+	g_GameObject.push_back(field);
+
+	Player* player = new Player();
+	player->Init();
+	g_GameObject.push_back(player);
+
+	Polygon2D* polygon2D = new Polygon2D();
+	polygon2D->Init();
+	g_GameObject.push_back(polygon2D);
+
+
+
 }
 
 void ManagerT::Uninit()
 {
-	for (int i = 0; i < GAMEOBJECT_TYPE::MAX; i++) {
+	/*for (int i = 0; i < GAMEOBJECT_TYPE::MAX; i++) {
 		g_pGameObject[i]->Uninit();
 		delete g_pGameObject[i];
+	}*/
+	for (GameObject* object : g_GameObject) {
+		object->Uninit();
+		delete object;
 	}
+	g_GameObject.clear();	// リストのクリア
+
 	Renderer::Uninit();
 }
 
@@ -98,8 +126,12 @@ void ManagerT::Update()
 	g_polygon->Update();
 	g_Player->Update();*/
 
-	for (int i = 0; i < GAMEOBJECT_TYPE::MAX; i++) {
+	/*for (int i = 0; i < GAMEOBJECT_TYPE::MAX; i++) {
 		g_pGameObject[i]->Update();
+	}*/
+
+	for (GameObject* object : g_GameObject) {
+		object->Update();
 	}
 
 }
@@ -111,9 +143,11 @@ void ManagerT::Draw()
 	g_Field->Draw();
 	g_Player->Draw();
 	g_polygon->Draw();*/
-	for (int i = 0; i < GAMEOBJECT_TYPE::MAX; i++) {
+	/*for (int i = 0; i < GAMEOBJECT_TYPE::MAX; i++) {
 		g_pGameObject[i]->Draw();
+	}*/
+	for (GameObject* object : g_GameObject) {
+		object->Draw();
 	}
-
 	Renderer::End();
 }
