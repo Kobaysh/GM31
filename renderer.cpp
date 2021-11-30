0fff723ea7e1a681c35fedb13fa0314d536beb15
Kobaysh <73707817+Kobaysh@users.noreply.github.com>
2021-11-30 10:53:29 +0900

    add:Filter change AnisotropicFilterling

--- renderer.cpp
+++ renderer.cpp
@@ -221,13 +221,15 @@ void Renderer::Init()
 
 	// サンプラーステート設定
 	D3D11_SAMPLER_DESC samplerDesc{};
-	samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+//	samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+	samplerDesc.Filter = D3D11_FILTER_ANISOTROPIC;
 	samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
 	samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
 	samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
 	samplerDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
 	samplerDesc.MinLOD = 0;
 	samplerDesc.MaxLOD = D3D11_FLOAT32_MAX;
+	samplerDesc.MaxAnisotropy = 16;
 
 	ID3D11SamplerState* samplerState = nullptr;
 //	m_Device->CreateSamplerState( &samplerDesc, &samplerState );
